ef36f55487f2dba0db740cb2e9e21dcf2c2518a4
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-04-04 19:56:22 +0200

    Initialized the project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    index: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "[name].bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/template.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
