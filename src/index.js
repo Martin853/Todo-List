8f5f25b149e006be39849fb23bd921baf66faed8
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-04-05 19:08:32 +0200

    Created the functionality for opening and closing the modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+// Imports
 import "./styles/main.scss";
+
+// Variables
+
+const addTask = document.querySelector("#add-task");
+const modal = document.querySelector("#modal");
+const cancelButton = document.querySelector("#cancel");
+
+// Task List
+
+// Add Task Button
+
+addTask.addEventListener("click", function () {
+  modal.style.display = "flex";
+});
+
+// Cancel Button
+
+cancelButton.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
