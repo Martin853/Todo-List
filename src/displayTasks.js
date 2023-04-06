9fa6ec574ccbdcb9a8f1b65d87a211b7b6f209cb
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-04-06 19:57:20 +0200

    Created add task functionality

---
+++ src/displayTasks.js
@@ -0,0 +1,46 @@
+export function displayTasks(list, taskList) {
+  // Task Container Variable
+
+  const taskContainer = document.querySelector("#task-container");
+
+  // Empty Task Container
+
+  taskContainer.innerHTML = "";
+
+  // Append the task div
+
+  for (let i = 0; i < list.length; i++) {
+    const taskDiv = document.createElement("div");
+    taskDiv.id = "task";
+    const textContent = document.createElement("div");
+    const taskTitle = document.createElement("h1");
+    taskTitle.innerText = list[i].taskTitle;
+    const taskDescription = document.createElement("p");
+    taskDescription.innerText = list[i].taskDescription;
+    const taskDueDate = document.createElement("p");
+    taskDueDate.innerText = list[i].dueDate;
+    const buttonGroup = document.createElement("div");
+    const editButton = document.createElement("button");
+    editButton.innerText = "Edit";
+    editButton.classList = "edit";
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.classList = "delete";
+
+    // Text Content Group
+
+    textContent.append(taskTitle, taskDescription, taskDueDate);
+
+    // Button Group
+
+    buttonGroup.append(editButton, deleteButton);
+
+    // Task Div
+
+    taskDiv.append(textContent, buttonGroup);
+
+    // Task Container
+
+    taskContainer.appendChild(taskDiv);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Imports
-import { TodoList } from "./constructor";
+import { Task, TodoList } from "./constructor";
+import { displayTasks } from "./displayTasks";
 import "./styles/main.scss";
 
 // Variables
@@ -7,6 +8,10 @@ import "./styles/main.scss";
 const addTask = document.querySelector("#add-task");
 const modal = document.querySelector("#modal");
 const cancelButton = document.querySelector("#cancel");
+const saveButton = document.querySelector("#save");
+const taskTitleInput = document.querySelector("#task-title");
+const taskDescriptionInput = document.querySelector("#task-description");
+const taskDueDateInput = document.querySelector("#task-date");
 
 // Task List
 
@@ -29,3 +34,33 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+// Save Button
+
+saveButton.addEventListener("click", function () {
+  // Getting the user Input
+  const title = taskTitleInput.value;
+  const description = taskDescriptionInput.value;
+  const dueDate = taskDueDateInput.value;
+
+  // Creating and adding the task to the task list
+  const task = new Task(title, description, dueDate);
+  taskList.addTask(task);
+  console.log(taskList);
+
+  // Clearing the input fields and closing the modal
+  taskTitleInput.value = "";
+  taskDescriptionInput.value = "";
+  taskDueDateInput.value = "";
+  modal.style.display = "none";
+
+  // Update the list
+
+  updateTaskList();
+
+  console.log(TodoList);
+});
+
+function updateTaskList() {
+  displayTasks(taskList.tasks, taskList);
+}
